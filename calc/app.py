e89007fd55b915236a1bbb50c07ef7900738135a
mike <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-06-10 21:50:24 -0400

    final revision

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,31 @@
 # Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def adding():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(add(a, b))
+
+@app.route('/sub')
+def subtract():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(sub(a, b))
+
+@app.route('/mult')
+def multiply():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(mult(a, b))
+
+@app.route('/div')
+def dividing():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    return str(div(a, b))
+
